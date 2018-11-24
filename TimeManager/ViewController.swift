e3308df5e356dab1fe8e119bbb83459155cd3230
zhangyingjie <kylinzhangyingjie@gmail.com>
2018-11-24 09:59:09 +0800

    Initial Commit

---
+++ TimeManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeManager
+//
+//  Created by 张英杰 on 2018/11/24.
+//  Copyright © 2018年 张英杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
